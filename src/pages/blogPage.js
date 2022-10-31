8f92ea07b47d34decfa44a5490751a66e0e49419
KubaSkrocki <37295334+KubaSkrocki@users.noreply.github.com>
2022-10-31 16:59:46 +0100

    Update blogPage.js

--- src/pages/blogPage.js
+++ src/pages/blogPage.js
@@ -1,23 +1,23 @@
 
-    import React from "react"
-    import {​​ graphql }​​ from "gatsby"
+    // import React from "react"
+    // import {​​ graphql }​​ from "gatsby"
     
-    const Page = ({​​ data }​​) => <pre>{​​JSON.stringify(data, null, 4)}​​</pre>
-    export const query = graphql`
-    query {​​
-    allContentfulBlogPost {​​
-    edges {​​
-    node {​​
-    slug
-    title
-    image {​​
-    alt
-    gatsbyImageData
-    }​​
-    }​​
-    }​​
-    }​​
-    }​​
-    `
-    export default Page
+    // const Page = ({​​ data }​​) => <pre>{​​JSON.stringify(data, null, 4)}​​</pre>
+    // export const query = graphql`
+    // query {​​
+    // allContentfulBlogPost {​​
+    // edges {​​
+    // node {​​
+    // slug
+    // title
+    // image {​​
+    // alt
+    // gatsbyImageData
+    // }​​
+    // }​​
+    // }​​
+    // }​​
+    // }​​
+    // `
+    // export default Page
     
