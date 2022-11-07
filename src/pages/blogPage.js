58e5349c27a72bb172c6fa7882ba6fc775e26ac4
KubaSkrocki <37295334+KubaSkrocki@users.noreply.github.com>
2022-11-07 14:01:21 +0100

    Try#1
    Read and study a lot this weekend, and i'm trying to do something which will work.

--- src/pages/blogPage.js
+++ src/pages/blogPage.js
@@ -1,17 +1,49 @@
-import React from "react";
-import Link from "gatsby-link";
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
-import { graphql } from "gatsby"
-// import PostLink from "../templates/blog-post.js"
+// import blogStyles from "./blog.module.scss"
 
-const Page = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-);
 
-export default Page;
+const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulBlogPost(sort: { fields: publishDate, order: DESC }) {
+        edges {
+          node {
+            title
+            slug
+            publishDate(fromNow: true)
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <main>
+        <h1>Blog</h1>
+        <ol>
+          {data.allContentfulBlogPost.edges.map(edge => {
+            return (
+
+              <li>
+                <Link to={`/blog/${edge.node.slug}`}>
+                  <div>
+                    <h2>{edge.node.title}</h2>
+                    <p>{edge.node.publishDate}</p>
+                  </div>
+                </Link>
+              </li>
+
+            )
+          })}
+        </ol>
+      </main>
+    </Layout>
+  )
+}
+
+export default BlogPage
     
