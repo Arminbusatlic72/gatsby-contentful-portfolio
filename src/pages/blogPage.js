2e1d7c4e4cb74eb75a2a36836e2e83e8308113e1
KubaSkrocki <37295334+KubaSkrocki@users.noreply.github.com>
2022-11-17 06:46:57 +0100

    Update blogPage.js

--- src/pages/blogPage.js
+++ src/pages/blogPage.js
@@ -7,25 +7,50 @@ const BlogPage = ({ data }) => {
 const posts = data.allContentfulBlogPost.nodes
 console.log(posts);
   return (
+    // <Layout>
+    //   <ul>
+    //     {posts.map(post => (
+    //       <div>
+    //         <Link to={`/blog/${post.slug}`}>
+    //             <li key={post.id}>
+    //               <h2>{post.title}</h2>
+    //               <GatsbyImage
+    //                 alt={post.image.alt}
+    //                 image={getImage(post.image.gatsbyImageData)}
+    //               />
+    //             </li>
+    //           </Link>
+    //       </div>
+    //     ))}
+    //   </ul>
+    // </Layout>
     <Layout>
-      <ul>
-        {posts.map(post => (
-          <div>
-            <Link to={`/blog/${post.slug}`}>
-                <li key={post.id}>
-                  <h2>{post.title}</h2>
-                  <GatsbyImage
-                    alt={post.image.alt}
-                    image={getImage(post.image.gatsbyImageData)}
-                  />
-                </li>
-              </Link>
-          </div>
-        ))}
-      </ul>
-    </Layout>
-  )
-}
+       <ul>
+         {posts.map(post => (
+          <Link to={`/blog/${post.slug}`}>
+              <Box width="third" padding={4} center>
+              {post.image && (
+                <GatsbyImage
+                  alt={post.image.alt}
+                  image={getImage(post.image.gatsbyImageData)}
+                />
+              )}
+              <Space size={3} />
+              <Box>
+                {post.title && (
+                  <Text variant="medium" bold center>
+                    {post.title}
+                  </Text>
+                )}
+              </Box>
+            </Box>
+        </Link>
+    ))}}
+       </ul>
+   </Layout>
+
+
+
 
 export const query = graphql`
   query {
