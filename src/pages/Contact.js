ce075c3222e294a11e0b71e38f16d322d5a856ed
KubaSkrocki <37295334+KubaSkrocki@users.noreply.github.com>
2022-12-12 17:09:43 +0100

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,122 +1,82 @@
 import React, { useState } from "react"
 import { graphql, Link } from "gatsby"
-import axios from "axios";
 import Layout from "../components/layout"
 import { Heading, Button, Box, Section, Container, Text, Kicker } from "../components/ui"
 
 const FORM_ENDPOINT = "";
 
 const ContactForm = () => {
-  const [status, setStatus] = useState();
-  const handleSubmit = (e) => {
-    e.preventDefault();
 
-    const injectedData = {
-      DYNAMIC_DATA_EXAMPLE: 123,
-    };
-    const inputs = e.target.elements;
-    const data = {};
-
-    for (let i = 0; i < inputs.length; i++) {
-      if (inputs[i].name) {
-        data[inputs[i].name] = inputs[i].value;
-      }
-    }
-
-    Object.assign(data, injectedData);
-
-    fetch(FORM_ENDPOINT, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
+    const [formState, setFormState] = useState({
+      name: "",
+      email: ""
     })
-      .then((response) => {
-        if (response.status === 422) {
-          Object.keys(injectedData).forEach((key) => {
-            const el = document.createElement("input");
-            el.type = "hidden";
-            el.name = key;
-            el.value = injectedData[key];
 
-            e.target.appendChild(el);
-          });
-
-          e.target.submit();
-          throw new Error("Please finish the captcha challenge");
-        }
-
-        if (response.status !== 200) {
-          throw new Error(response.statusText);
-        }
-
-        return response.json();
+    const handleChange = e => (
+      setFormState({
+        ...formState,
+        [e.target.name]: e.tagret.value,
       })
-      .then(() => setStatus("We'll be in touch soon."))
-      .catch((err) => setStatus(err.toString()));
-  };
+    )
+    
+    const handleSubmit = e => (
+      e.preventDefault()
+    )
 
-  if (status) {
     return (
-      <>
-        <Box>Thank you!</Box>
-        <Text center>{status}</Text>
-      </>
-    );
-  }
 
-  return (
-
-    <Layout>
-        <Section>
-            <Container width="tight" center>
-                    <Box  padding={1} center>
-                        <Heading>CONTACT</Heading>
-                    </Box>
-                        <form
-                        action={FORM_ENDPOINT}
-                        onSubmit={handleSubmit}
-                        method="POST"
-                        target="_blank"
-                        >
-                        <Box  padding={2} center>
-                            <input
-                            type="text"
-                            placeholder="Your name"
-                            name="name"
-                            class="form-control"
-                            required
-                            />
-                        </Box>
-                        <Box  padding={2} center>
-                            <input
-                            type="email"
-                            placeholder="Email"
-                            name="email"
-                            class="form-control"
-                            required
-                            />
-                        </Box>
-                        <Box className="form-group" padding={1} center>
-                            <textarea
-                            placeholder="Your message"
-                            name="message"
-                            class="form-control form-control-lg"
-                            required
-                            />
-                        </Box>
+        <Layout>
+            <Section>
+                <Container width="tight" center>
                         <Box  padding={1} center>
-                            <Button type="submit">
-                            Send a message
-                            </Button>
+                            <Heading>CONTACT</Heading>
                         </Box>
-                        </form>
-            </Container>
-        </Section>
-    </Layout>
-  );
-};
+                            <form
+                            method="POST"
+                            target="_blank"
+                            data-netlify="true"
+                            netlify_honeypot="bot-field"
+                            >
+                            <Box  padding={2} center>
+                                <input
+                                type="text"
+                                placeholder="Your name"
+                                name="name"
+                                class="form-control"
+                                onChange={handleChange}
+                                value={formState.name}
+                                required
+                                />
+                            </Box>
+                            <Box  padding={2} center>
+                                <input
+                                type="email"
+                                placeholder="Email"
+                                name="email"
+                                class="form-control"
+                                onChange={handleChange}
+                                value={formState.email}
+                                required
+                                />
+                            </Box>
+                            <Box className="form-group" padding={1} center>
+                                <textarea
+                                placeholder="Your message"
+                                name="message"
+                                class="form-control form-control-lg"
+                                required
+                                />
+                            </Box>
+                            <Box  padding={1} center>
+                                <Button type="submit">
+                                Send a message
+                                </Button>
+                            </Box>
+                            </form>
+                </Container>
+            </Section>
+        </Layout>
+      )
+}
 
 export default ContactForm;
